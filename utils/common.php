ff0d473d421908229304ed3e7f3ac9fffbc8bdc0
Alex Psi <alexpsi256@gmail.com>
2017-07-26 18:49:10 +0300

    Replace dpm with drupal set message

--- utils/common.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function extractSlugFromUrl($url) {
-  $exploded = array_filter(explode('/', $url));
-  if (strcmp(end($exploded), 'dashboard') == 0) {
-    array_pop($exploded);
-  }
-  return end($exploded);
-}
